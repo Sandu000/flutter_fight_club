598b6037198d0e6faecd8e65405234b364eeff43
e-Sandugash.Ryskulbek <e-sandugash.ryskulbek@MacBook-Pro-e-SandugashRyskulbek.local>
2023-06-01 10:33:09 +0600

    hw3

--- lib/fight_club_icons.dart
+++ lib/fight_club_icons.dart
@@ -2,5 +2,5 @@ class FightClubIcons {
   FightClubIcons._();
 
   static const heartEmpty = "assets/icons/heart-empty.png";
-  static const heartFull = "assets/icons/heart-filled.png";
+  static const heartFull = "assets/icons/heart-full.png";
 }
